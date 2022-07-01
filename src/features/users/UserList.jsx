a3b7474e3593f2a79b771a863ce84aec0007cb10
iamfaizan <faizanalee@yahoo.com>
2022-07-01 15:22:20 +0530

    initial files

---
+++ src/features/users/UserList.jsx
@@ -0,0 +1,74 @@
+import { fetchUsers, userDeleted } from "./usersSlice";
+import { useDispatch, useSelector } from "react-redux";
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import '../../../src/App.css';
+
+import { Link } from "react-router-dom";
+
+export function UserList() {
+  const dispatch = useDispatch();
+
+  const { entities } = useSelector((state) => state.users);
+  const loading = useSelector((state) => state.loading);
+
+  const handleDelete = (id) => {
+    dispatch(userDeleted({ id }));
+  };
+
+  return (
+    <div className="container">
+      <div className="row" style={{"background-color": "#dee7f6"}}>
+        <h1>The list of users : </h1>
+      </div>
+      <br></br>
+      <div className="row">
+        <div className="two columns">
+          <button
+            onClick={() => dispatch(fetchUsers())}
+            className="button-primary"
+          >
+            Load users
+          </button>
+        </div>
+        <div className="two columns">
+          <Link to="/add-user">
+            <button className="button-primary">Add user</button>
+          </Link>
+        </div>
+      </div>
+      <div className="row">
+        {loading ? (
+          "Loading..."
+        ) : (
+          <table className="table u-full-width" >
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {entities.length &&
+                entities.map(({ id, name, email }, i) => (
+                  <tr key={i}>
+                    <td>{id}</td>
+                    <td>{name}</td>
+                    <td>{email}</td>
+                    <td>
+                    <button style = {{"border": "none"}} onClick={() => handleDelete(id)}><DeleteIcon/></button>
+                      <Link to={`/edit-user/${id}`}>
+                        <EditIcon></EditIcon>
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </div>
+  );
+}
